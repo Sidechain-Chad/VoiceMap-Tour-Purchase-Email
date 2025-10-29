1010f54a3724493d145777a5d7cf47df1185f600
Chad Niewenhuis <chadn94@gmail.com>
2025-10-30 00:21:21 +0200

    Completed tables layout
    Before compliant batch code

--- test/mailers/previews/tour_mailer_preview.rb
+++ test/mailers/previews/tour_mailer_preview.rb
@@ -3,7 +3,9 @@ class TourMailerPreview < ActionMailer::Preview
     route = OpenStruct.new(
       title: "Seward Highway Tour",
       price: 19.99,
-      destination: "Anchorage, Alaska"
+      destination: "Anchorage, Alaska",
+      image: "WP_20140920_16_46_41_Pro_1744591093.png",
+      directions: "The tour begins at the large parking area for Potter Marsh Wildlife Viewing Boardwalk, about 15 minutes south of downtown Anchorage. To get there, drive southbound on the Seward Highway past the exit for Rabbit Creek Road/Old Seward Highway. As you leave Anchorage, watch for a large brown sign marking the Anchorage Coastal Wildlife Refuge, with arrows pointing left for Potter Marsh Boardwalk and right for Rabbit Creek Rifle Range. Use the left-turning lane to take the Seward Highway turnoff for Potter Marsh access road, which begins at East 154th Avenue and leads to the parking area at the end."
     )
     user = OpenStruct.new(email: 'test@example.com')
     TourMailer.tour_added(user, route)
